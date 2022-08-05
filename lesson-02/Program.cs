2ad62a94a730c2235e44ff304929284f7dd83be2
rramii <47689248+rramii@users.noreply.github.com>
2022-08-05 21:18:19 +0300

    work in progress

--- lesson-02/Program.cs
+++ lesson-02/Program.cs
@@ -2,9 +2,72 @@
 {
     internal class Program
     {
+        static void pprint(string[][] matrix)
+        {
+            Console.WriteLine("{");
+            for (int n = 0; n < matrix.Length; n++)
+            {
+                if (matrix[n].Length > 0)
+                {
+                    Console.Write($" [ {matrix[n][0]}");
+                }
+                for (int k = 1; k < matrix[n].Length; k++)
+                {
+                    Console.Write($" , {matrix[n][k]}");
+                }
+                Console.WriteLine(" ] ");
+            }
+            Console.WriteLine("}");
+        }
+
+        static string check_nieghbors(string[][] main_cell)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                string nieghbors = { main_cell[][], main_cell[][], main_cell[][], main_cell[][] };
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("begin");
+
+            string[][] cells = new string[15][] 
+            {
+                new string[] { "_" , "_"  , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }, 
+                new string[] { "_" , "_"  , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }, 
+                new string[] { "_" , "_"  , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }, 
+                new string[] { "_" , "_"  , "_" , "_" , "#" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }, 
+                new string[] { "_" , "_"  , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }, 
+                new string[] { "_" , "_"  , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }, 
+                new string[] { "_" , "_"  , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }, 
+                new string[] { "_" , "_"  , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }, 
+                new string[] { "_" , "_"  , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }, 
+                new string[] { "_" , "_"  , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }, 
+                new string[] { "_" , "_"  , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }, 
+                new string[] { "_" , "_"  , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }, 
+                new string[] { "_" , "_"  , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }, 
+                new string[] { "_" , "_"  , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }, 
+                new string[] { "_" , "_"  , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , "_" , }
+            };
+
+            for (int i = 0; i < cells.Length; i++)
+            {
+                for (int k = 0; k < cells[i].Length; k++)
+                {
+                    string cel = cells[i][k];
+                    while (cel == "#")
+                    {
+                        string nieghbors = check_nieghbors(cells[i][k]);
+                        if (nieghbors.Length > 3 || nieghbors.Length < 2)
+                        {
+                            string cel = "_";
+                        }
+                    }
+
+                }
+            }
+            pprint(cells);
         }
     }
 }
