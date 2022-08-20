d622490444c7042f7b6b72a07e3caa21c59883f9
rramii <47689248+rramii@users.noreply.github.com>
2022-08-20 10:45:53 +0300

    old

---
+++ lesson-03/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace lesson_03
+{
+    internal class Life
+    {
+        private CellState[,] matrix;
+        public Life(int rows, int columns)
+        {
+            matrix = new CellState[rows, columns];
+        }
+
+        public void next_generation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void turnon(int row, int col)
+        {
+            //TODO: check row/col are legal
+            matrix[row, col] = CellState.IsAlive;
+        }
+
+        public void turnof(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void print()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
