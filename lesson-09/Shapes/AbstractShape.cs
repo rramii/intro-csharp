e09cbc94762faa447ca72f5566b0bbfdb9ae569a
Muhammad Zahalqa <m@tryfinally.com>
2022-08-23 18:54:24 +0300

    Polymorphism lab

---
+++ lesson-09/Shapes/AbstractShape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public abstract class AbstractShape
+    {
+        public abstract string Name { get; }
+        public abstract double Area();
+
+        public double PaintVolume(double thickness)
+        {
+            var s = Area();
+            return s * thickness;
+        }
+    }
+}
