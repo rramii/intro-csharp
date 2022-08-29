8dbb43d7701aaac427b2302dc925ec24be34e1a4
rramii <47689248+rramii@users.noreply.github.com>
2022-08-29 13:36:47 +0300

    all that i got

--- lesson-10/SortedList.cs
+++ lesson-10/SortedList.cs
@@ -12,27 +12,49 @@ namespace lesson_10
         private List<double> _list = new List<double>();
 
         public bool Remove(double x)
-                {
-                    var before = Count();
-                    var here = FindLocation(x);
-                    _list.RemoveAt(here);
-                    return Count() < before;
-                }
+        {
+            var before = Count();
+            var here = FindLocation(x);
+            _list.RemoveAt(here[0]);
+            return Count() < before;
+        }
 
         public bool Add(double x)
         {
             var before = Count();
-            var here = FindLocation(x);
-            _list.Insert(here, x);            
+            _list.Append(x);
+            int[] here = FindLocation(x);
+
+            if (_list.Count >= 2)
+            {
+                int[] swap = Swap((int)_list[Count() - 1], here[0]);
+                Move(swap, (int)x, here[1]);
+            }
             return Count() > before;
         }
 
-        private int FindLocation(double x)
+        private int[] Swap(int intruderL, int oldnumL)
         {
-            
+            intruderL = intruderL * oldnumL;
+            oldnumL = intruderL / oldnumL;
+            intruderL = intruderL / oldnumL;
+            int[] locations = { intruderL, oldnumL };
+            return locations;
+        }
+
+        private void Move(int[] locList, int x, int oldnum)
+        {
+            locList[0] = x;
+            locList[1] = oldnum;
+        }
+
+        private int[] FindLocation(double x)
+        {
+            int[] counter = {Count(), Count()};
+
             if (Count() == 0)
             {
-                return Count();
+                return counter;
             }
 
             int low = 0;
@@ -42,6 +64,7 @@ namespace lesson_10
             {
                 int mid = (low + high) / 2;
                 var element = Get(mid);
+
                 if (x > element)
                 {
                     low = mid + 1;
@@ -52,11 +75,14 @@ namespace lesson_10
                 }
                 else
                 {
-                    return mid;
+                    int[] idk = { mid, (int) element };
+                    return idk;
                 }
             }
-            return Count();
+            return counter;
         }
+
+        private int[] transfer(int[] list) => list;
 
         public int Count() => _list.Count;
         
@@ -65,7 +91,5 @@ namespace lesson_10
         public double Max() => _list[Count()-1];
         
         public double Min() => _list[0];
-
-        public void Sorting(List<int> new_list) => _list.Sort();
     }
 }
