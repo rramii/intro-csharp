06f1d265270ba2374f0ebcd26616b856c11a483f
Muhammad Zahalqa <m@tryfinally.com>
2022-09-08 19:33:01 +0300

    Master calc

--- lesson-11/Form1.cs
+++ lesson-11/Form1.cs
@@ -12,43 +12,44 @@ namespace lesson_11
 {
     public partial class Form1 : Form
     {
+        private StackCalc calc = new StackCalc();
+        
+
         public Form1()
         {
             InitializeComponent();
+            calc.OnPopItems = popList;
+            calc.OnPushItem = pushList;
+            calc.OnProblem = displayError;
         }
 
-        private void excuteoperation(Func<int,int,int> op)
+        private void displayError(string msg)
         {
-            if (listBox1.Items.Count > 1)
-            {
-                var index = listBox1.Items.Count - 1;
-                var lastnum = listBox1.Items[index];
-                var beforelast = listBox1.Items[index - 1];
-
-                var a = Convert.ToInt32(lastnum);
-                var b = Convert.ToInt32(beforelast);
-                var result = op(a, b);
-
-                listBox1.Items.RemoveAt(index);
-                listBox1.Items.RemoveAt(index - 1);
-
-                listBox1.Items.Add(result);
-            }
-            else
-            {
-                label1.Text = "need 2 numbers";
-            }
+            label1.Text = msg;
+        }
+        private void pushList(int n)
+        {
+            listBox1.Items.Add(n.ToString());
         }
 
+        private void popList(int n)
+        {
+            var items = listBox1.Items;
+            for (int i = 0; i < n; i++)
+            {
+                items.RemoveAt(items.Count - 1);
+            }            
+        }
+
+        
         private void Pushbtn(object sender, EventArgs e)
         {
             var text = textBox1.Text;
             int number;
             if (int.TryParse(text, out number))
             {
-                listBox1.Items.Add(number);
+                calc.Push(number);
                 textBox1.Text = "";
-                label1.Text = "";
             }
             else
             {
@@ -56,112 +57,40 @@ namespace lesson_11
             }
         }
 
-        private void Addbtn(object sender, EventArgs e)
-        {
-            excuteoperation((a,b) => a + b);
-        }
-        private void Subtractbtn(object sender, EventArgs e)
-        {
-            excuteoperation((a,b) => b - a);
-        }
+        private void Addbtn(object sender, EventArgs e) => calc.Add();
+        
+        private void Subtractbtn(object sender, EventArgs e) => calc.Sub();
 
-        private void Multibtn(object sender, EventArgs e)
-        {
-            excuteoperation((a, b) => a * b);
-        }
+        private void Multibtn(object sender, EventArgs e) => calc.Mul();
 
-        private void Divbtn(object sender, EventArgs e)
-        {
-            excuteoperation((a, b) => b / a);
-        }
+        private void Divbtn(object sender, EventArgs e) => calc.Div();
 
-        private void Modulebtn(object sender, EventArgs e)
-        {
-            excuteoperation((a, b) => a % b);
-            excuteoperation((a, b) => b / a);
+        private void Modulebtn(object sender, EventArgs e) => calc.Mod();
+        
 
-        }
+        private void Powerbtn(object sender, EventArgs e) => calc.Power();
 
-        private void Powerbtn(object sender, EventArgs e)
-        {
-            excuteoperation((a, b) => (int)Math.Pow(a, b));
-        }
 
-        private void Sqrtbtn(object sender, EventArgs e)
-        {
-            var index = listBox1.Items.Count - 1;
-            var lastnum = listBox1.Items[index];
-            var a = Convert.ToInt32(lastnum);
+        private void Sqrtbtn(object sender, EventArgs e) => calc.Sqrt();
+        
 
-            if (a > 0)
-            {
-                var b = Math.Sqrt(a);
-                listBox1.Items.RemoveAt(index);
-                listBox1.Items.Add(b);
-            }
-            else
-            {
-                label1.Text = "number has to be positive";
-            }
-        }
+        private void Absolute(object sender, EventArgs e) => calc.Abs();
 
-        private void Absolute(object sender, EventArgs e)
-        {
-            var index = listBox1.Items.Count - 1;
-            var lastnum = listBox1.Items[index];
-            var a = Convert.ToInt32(lastnum);
-            if (a < 0)
-            {
-                listBox1.Items[index] = -a;
-            }
-        }
+        private void Clear(object sender, EventArgs e) => calc.Reset();
+        
 
-        private void Clear(object sender, EventArgs e)
-        {
-            listBox1.Items.Clear();
-        }
+        private void Swap(object sender, EventArgs e) => calc.Swap();
 
-        private void Swap(object sender, EventArgs e)
-        {
-            if (listBox1.Items.Count >= 3)
-            {
-                var index = listBox1.Items.Count - 1;
-                var lastnum = listBox1.Items[index];
-                var beforelast = listBox1.Items[index - 1];
-                var thirdtolast = listBox1.Items[index - 2];
-
-                var a = Convert.ToInt32(thirdtolast);
-                var b = Convert.ToInt32(beforelast);
-                var c = Convert.ToInt32(lastnum);
-                
-                listBox1.Items[index - 2] = c;
-                listBox1.Items[index - 1] = a;
-                listBox1.Items[index] = b;
-
-            }
-            else
-            {
-                label1.Text = "3 numbers needed";
-            }
-        }
+        private void button12_Click(object sender, EventArgs e) => calc.Rotate(3);
 
         private void CheckEnter(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == Convert.ToChar(Keys.Enter))
             {
-                var text = textBox1.Text;
-                int number;
-                if (int.TryParse(text, out number))
-                {
-                    listBox1.Items.Add(number);
-                    textBox1.Text = "";
-                    label1.Text = "";
-                }
-                else
-                {
-                    label1.Text = "not a number";
-                }
+                button1.PerformClick();
             }
         }
+
+        private void button13_Click(object sender, EventArgs e) => calc.DivRem();
     }
 }
---
+++ lesson-11/StackCalc.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_11
+{
+
+    public class StackCalc
+    {
+        private Stack<int> _stack = new Stack<int>();
+
+        public Action<int> OnPopItems;
+        public Action<int> OnPushItem;
+        public Action<string> OnProblem;
+
+        public void Push(int n)
+        {
+            _stack.Push(n);
+            OnPushItem(n);
+        }
+        
+        private void executeBinary(Func<int, int, int> op)
+        {
+            if (dontHave(2))
+            {
+                return;
+            }
+            var a = _stack.Pop();
+            var b = _stack.Pop();
+            OnPopItems(2);
+
+            var r = op(a, b);
+            Push(r);            
+            OnProblem("OK");
+        }
+
+        private void executeUniary(Func<int, int> op)
+        {
+            if (dontHave(1))
+            {
+                return;
+            }
+            var a = _stack.Pop();            
+            OnPopItems(1);
+
+            var r = op(a);
+            Push(r);
+            OnProblem("OK");
+        }
+
+        
+        public void Add() => executeBinary((a, b) => a + b);
+        public void Mul() => executeBinary((a, b) => a * b);
+
+        internal void Sub() => executeBinary((a, b) => b - a);
+        internal void Div() => executeBinary((a, b) => b / a);
+
+        internal void DivRem()
+        {
+            if (dontHave(2))
+            {
+                return;
+            }
+            var a = _stack.Pop();
+            var b = _stack.Pop();
+            OnPopItems(2);
+
+            var r = b / a;
+            var m = b % a;
+            Push(r);
+            Push(m);
+            OnProblem("OK");
+        }
+
+        internal void Power() => executeBinary((a, b) => (int)Math.Pow(a, b));
+
+        internal void Abs() => executeUniary(x => Math.Abs(x));
+
+        internal void Sqrt() => executeUniary(x => (int)Math.Sqrt(x));
+
+        internal void Swap()
+        {
+            
+            var a = _stack.Pop();
+            var b = _stack.Pop();
+            OnPopItems(2);
+
+            Push(a);
+            Push(b);
+            OnProblem("OK");
+        }
+
+        internal void Mod() => executeBinary((a, b) => b % a);
+
+        internal void Rotate(int v)
+        {
+            if (dontHave(3))
+            {
+                return;
+            }
+            var a = _stack.Pop();
+            var b = _stack.Pop();
+            var c = _stack.Pop();
+            OnPopItems(3);
+
+            Push(a);
+            Push(c);
+            Push(b);
+            OnProblem("OK");
+        }
+
+        private bool dontHave(int needed)
+        {
+            if (_stack.Count < needed)
+            {
+                OnProblem($"{needed} needed");
+                return true;
+            }
+            return false;
+        }
+
+        internal void Reset()
+        {
+            OnPopItems(_stack.Count);
+            _stack.Clear();
+        }
+    }
+}
